b68bc13b5956ad805348cb672aae230313477ea6
andmccutchan <andmccutchan2004@gmail.com>
2026-02-05 15:13:24 -0500

    <key, message> tuple code

--- project1_part1.py
+++ project1_part1.py
@@ -1,6 +1,6 @@
 import random
 
-seed = 01975251
+seed = 1965708
 random.seed(seed)
 
 key = random.getrandbits(128)
@@ -8,5 +8,12 @@ key = random.getrandbits(128)
 def text_to_bits(text: str, encoding="utf-8") -> str:
     data = text.encode(encoding)
     return ''.join(f'{byte:08b}' for byte in data)
+
+message = text_to_bits("D01965708D01966276D01975251")[:128]
+encrypt_input = (key, message)
+
+print(encrypt_input)
+
+
 
 
