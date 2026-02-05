6fa08621b8f638c40d0b60de6340ac910e5a089c
andmccutchan <andmccutchan2004@gmail.com>
2026-02-05 16:31:00 -0500

    Added shift rows and message table

---
+++ message_tuple.py
@@ -0,0 +1,16 @@
+import random
+
+seed = 1965708
+random.seed(seed)
+
+key = random.getrandbits(128)
+
+def text_to_bits(text: str, encoding="utf-8") -> str:
+    data = text.encode(encoding)
+    return ''.join(f'{byte:08b}' for byte in data)
+
+message = text_to_bits("D01965708D01966276D01975251")[:128]
+
+
+
+
--- project1_part1.py
+++ project1_part1.py
@@ -1,18 +1,50 @@
-import random
+import message_tuple as mt
 
-seed = 1965708
-random.seed(seed)
+class AES:
+    def __init__(self, key, message):
+        self.key = key
+        self.message = message
+        
+    def message_table(self, message):
+        table = [[], [], [], []]
+        for i in range(0, len(message), 8):
+            byte = message[i:i+8]
+            row_index = i // 8 % 4
+            table[row_index].append(byte)
+        return table
+    
+    def shift_rows(self, table):
+        shifted_table = []
+        for i in range(4):
+            if i == 0:
+                shifted_table.append(table[i])
+            elif i == 1:
+                shifted_table.append(table[i][1:] + table[i][:1])
+            elif i == 2:
+                shifted_table.append(table[i][2:] + table[i][:2])
+            elif i == 3:
+                shifted_table.append(table[i][3:] + table[i][:3])
+        return shifted_table
+        
+    def encrypt(key, message):
+        # Placeholder for AES encryption logic
+        pass
+        
 
-key = random.getrandbits(128)
-
-def text_to_bits(text: str, encoding="utf-8") -> str:
-    data = text.encode(encoding)
-    return ''.join(f'{byte:08b}' for byte in data)
-
-message = text_to_bits("D01965708D01966276D01975251")[:128]
-encrypt_input = (key, message)
-
-print(encrypt_input)
+    
+   
+def main():
+    cipher = AES(mt.key, mt.message)
+    
+    cipher_table = cipher.message_table(cipher.message)
+    print("Cipher Table:")
+    print(cipher_table)
+    shifted_table = cipher.shift_rows(cipher_table)
+    print("Shifted Table:")
+    print(shifted_table)
+    
+if __name__ == "__main__":
+    main()
 
 
 
