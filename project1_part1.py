a145e2936615f5e580137eae3edbbc964c252d70
Lam Do <do_k2@denison.edu>
2026-02-09 11:39:33 -0500

    Polish project1_part1.py

--- project1_part1.py
+++ project1_part1.py
@@ -4,41 +4,52 @@ Authors: Andrew McCuthan, Eleanor, Lam Do
 Date: Februrary 5, 2026
 CS402, Spring 2026
 """
-
-import random
 import message_tuple as mt
 
 class AES:
     def __init__(self, key, message):
+        """Initializes the AES cipher with a key and message and create S-Box
+
+        Parameters: 
+            key (string): 128-bit key for encryption
+            message (string): 128-bit message to be encrypted
+
+        Returns: None
+        """
         self.key = key
         self.message = message
         self.sbox = self._create_sbox()
         
     def _create_sbox(self):
+        """Generates an S-box for nonlinear substitution
+        
+        Parameters: None
+
+        Returns: 
+            list: S-box as a 2D array of binary strings (16x16 matrix of 8-bit binary strings)
         """
-        generates an S-Box that will be used for the substitute bytes step of encryption rounds
-        """
+        # S-box values from textbook (hexadecimal)
         sbox = [
-        ["63","7C","77","7B","F2","6B","6F","C5","30","01","67","2B","FE","D7","AB","76"],
-        ["CA","82","C9","7D","FA","59","47","F0","AD","D4","A2","AF","9C","A4","72","C0"],
-        ["B7","FD","93","26","36","3F","F7","CC","34","A5","E5","F1","71","D8","31","15"],
-        ["04","C7","23","C3","18","96","05","9A","07","12","80","E2","EB","27","B2","75"],
-        ["09","83","2C","1A","1B","6E","5A","A0","52","3B","D6","B3","29","E3","2F","84"],
-        ["53","D1","00","ED","20","FC","B1","5B","6A","CB","BE","39","4A","4C","58","CF"],
-        ["D0","EF","AA","FB","43","4D","33","85","45","F9","02","7F","50","3C","9F","A8"],
-        ["51","A3","40","8F","92","9D","38","F5","BC","B6","DA","21","10","FF","F3","D2"],
-        ["CD","0C","13","EC","5F","97","44","17","C4","A7","7E","3D","64","5D","19","73"],
-        ["60","81","4F","DC","22","2A","90","88","46","EE","B8","14","DE","5E","0B","DB"],
-        ["E0","32","3A","0A","49","06","24","5C","C2","D3","AC","62","91","95","E4","79"],
-        ["E7","C8","37","6D","8D","D5","4E","A9","6C","56","F4","EA","65","7A","AE","08"],
-        ["BA","78","25","2E","1C","A6","B4","C6","E8","DD","74","1F","4B","BD","8B","8A"],
-        ["70","3E","B5","66","48","03","F6","0E","61","35","57","B9","86","C1","1D","9E"],
-        ["E1","F8","98","11","69","D9","8E","94","9B","1E","87","E9","CE","55","28","DF"],
-        ["8C","A1","89","0D","BF","E6","42","68","41","99","2D","0F","B0","54","BB","16"],
-    ]
+            ["63","7C","77","7B","F2","6B","6F","C5","30","01","67","2B","FE","D7","AB","76"],
+            ["CA","82","C9","7D","FA","59","47","F0","AD","D4","A2","AF","9C","A4","72","C0"],
+            ["B7","FD","93","26","36","3F","F7","CC","34","A5","E5","F1","71","D8","31","15"],
+            ["04","C7","23","C3","18","96","05","9A","07","12","80","E2","EB","27","B2","75"],
+            ["09","83","2C","1A","1B","6E","5A","A0","52","3B","D6","B3","29","E3","2F","84"],
+            ["53","D1","00","ED","20","FC","B1","5B","6A","CB","BE","39","4A","4C","58","CF"],
+            ["D0","EF","AA","FB","43","4D","33","85","45","F9","02","7F","50","3C","9F","A8"],
+            ["51","A3","40","8F","92","9D","38","F5","BC","B6","DA","21","10","FF","F3","D2"],
+            ["CD","0C","13","EC","5F","97","44","17","C4","A7","7E","3D","64","5D","19","73"],
+            ["60","81","4F","DC","22","2A","90","88","46","EE","B8","14","DE","5E","0B","DB"],
+            ["E0","32","3A","0A","49","06","24","5C","C2","D3","AC","62","91","95","E4","79"],
+            ["E7","C8","37","6D","8D","D5","4E","A9","6C","56","F4","EA","65","7A","AE","08"],
+            ["BA","78","25","2E","1C","A6","B4","C6","E8","DD","74","1F","4B","BD","8B","8A"],
+            ["70","3E","B5","66","48","03","F6","0E","61","35","57","B9","86","C1","1D","9E"],
+            ["E1","F8","98","11","69","D9","8E","94","9B","1E","87","E9","CE","55","28","DF"],
+            ["8C","A1","89","0D","BF","E6","42","68","41","99","2D","0F","B0","54","BB","16"],
+        ]
+        # Convert hexadecimal S-box to binary S-box
         for row in range(16):
             for col in range(16):
-                # Convert hexadecimal values to binary 8-bit values
                 hex = sbox[row][col]
                 dec = int(hex, 16)
                 binary = str(bin(dec)[2:])
@@ -51,28 +62,30 @@ class AES:
     def _message_table(self, message):
         """Turns the initial message into a 4x4 table used for the rounds
 
-        Args:
+        Parameters:
             message (string): original message to be encrypted
 
         Returns:
-            list: a 2D array of our message with 8-bit index values.
+            list: a 2D (4x4) array of our message with 8-bit index values.
         """
+        # Initialize empty 4x4 table
         table = [[], [], [], []]
+        # Fill in the table column by column with 8-bit segments of the message
         for i in range(0, len(message), 8):
             byte = message[i:i+8]
             row_index = i // 8 % 4
             table[row_index].append(byte)
         return table
     
-    def _sub_bytes(self, message, sbox):
-        """Substitution step of enctrpytion. Takes the original matrix and substitutes the bytes with our S-Box.
-
-        Args:
+    def _sub_bytes(self, message):
+        """Substitution step of encryption. Takes the original matrix and substitutes the bytes with our S-Box.
+        
+        Parameters:
             message (string): 4x4 Matrix table of message
             sbox (list): Our S-Box that will substitute the bytes
 
         Returns:
-            list: 4x4 Matrix table of substiuted bits
+            list: 4x4 Matrix table of substituted bits
         """
         output = []
         # Initilize new 128-bit message
@@ -87,13 +100,13 @@ class AES:
                 sbox_row = int(byte[:4], 2)
                 # Last 4 bits represent column index
                 sbox_col = int(byte[4:], 2)
-                output[row][col] = sbox[sbox_row][sbox_col]
+                output[row][col] = self.sbox[sbox_row][sbox_col]
         return output
     
     def _shift_rows(self, table):
-        """Shifting the rows of our table 
+        """Shifts the rows of our table 
 
-        Args:
+        Parameters:
             table (list): 4X4 matrix array. This will be the state after the substitution step.
 
         Returns:
@@ -101,20 +114,24 @@ class AES:
         """
         shifted_table = []
         for i in range(4):
+            # First row is not shifted
             if i == 0:
-                shifted_table.append(table[i]) # First row is not shifted
+                shifted_table.append(table[i])    
+            # Second row is shifted left by 1                  
             elif i == 1:
-                shifted_table.append(table[i][1:] + table[i][:1]) # Second row is shifted left by 1
+                shifted_table.append(table[i][1:] + table[i][:1])   
+            # Third row is shifted left by 2
             elif i == 2:
-                shifted_table.append(table[i][2:] + table[i][:2]) # Third row is shifted left by 2
+                shifted_table.append(table[i][2:] + table[i][:2])   
+            # You get the idea
             elif i == 3:
-                shifted_table.append(table[i][3:] + table[i][:3]) # You get the idea
+                shifted_table.append(table[i][3:] + table[i][:3])   
         return shifted_table
     
     def _mix_columns(self, state):
-        """This is performing the Mix column step of AES using GF multiplication.
+        """Performs the Mix column step of AES using GF multiplication.
 
-        Args:
+        Parameters:
             state (list): 4x4 matrix array. 
 
         Returns:
@@ -157,11 +174,15 @@ class AES:
         
     
     def _key_expansion(self, key):
+        """Generates the round keys for AES encryption using the key expansion algorithm
+        
+        Parameters:
+            key (string): 128-bit key for encryption
+
+        Returns:
+            list: An array of 44 words (each word is 32 bits)
         """
-        Input: 128-bit key
-        Output: Array w of 44 words
-        """
-        # Pull values from textbook
+        # Rcon values from textbook (hexadecimal)
         Rcon_hex = ["00", "01", "02", "04", "08", "10", "20", "40", "80", "1B", "36"]
         Rcon = [bin(int(hex,16))[2:].zfill(8) + "0" * 24 for hex in Rcon_hex]
 
@@ -179,10 +200,14 @@ class AES:
         return w
     
     def _xor(self, string1, string2):
-        """
-        This function performs a bitwise XOR operation on string1 and string2
-        Input: 2 strings of binary bits
-        Output: XOR string of string1 and string2
+        """Performs a bitwise XOR operation on two binary strings
+
+        Parameters:
+            string1 (string): First binary string
+            string2 (string): Second binary string
+
+        Returns:
+            string: Result of XOR operation as a binary string
         """
         xor_result = int(string1, 2) ^ int(string2, 2)
         xor_string = bin(xor_result)[2:]
@@ -190,10 +215,13 @@ class AES:
         return output
     
     def _sub_word(self, word):
-        """
-        This function performs byte substitution via S-box
-        Input: 4-byte word
-        Output: 4-byte word 
+        """Performs byte substitution on a 4-byte word using the S-box
+        
+        Parameters:
+            word (string): 4-byte word (32 bits) to be substituted
+
+        Returns:
+            string: Substituted 4-byte word (32 bits) after applying S-box
         """
         output = ""
         for i in range(0, 32, 8):
@@ -204,14 +232,26 @@ class AES:
         return output
 
     def _rot_word(self, word):
-        """
-        This function performs one-byte left shift [B0, B1, B2, B3] -> [B1, B2, B3, B0]
-        Input: 4-byte word
-        Output 4-byte word
+        """Performs one-byte left shift [B0, B1, B2, B3] -> [B1, B2, B3, B0]
+
+        Parameters:
+            word (string): 4-byte word (32 bits) to be rotated
+        
+        Returns:
+            string: Rotated 4-byte word (32 bits) after left shift
         """
         return word[8:] + word[:8]
     
     def encrypt(self, key, message):
+        """Encrypts the message using the AES encryption algorithm with the provided key
+        
+        Parameters:
+            key (string): 128-bit key for encryption
+            message (string): 128-bit message to be encrypted
+            
+        Returns:
+            list: 4x4 matrix array representing the encrypted message after 10 rounds of AES
+        """
         # Prepare state and round keys
         matrix_table = self._message_table(message)
         sbox = self._create_sbox()
@@ -244,26 +284,34 @@ class AES:
         state = self._add_round_key(state, round_keys[10])
 
         return state
-        
 
 def main():
+    """Main function to test AES encryption with the key and message from message_tuple.py"""
+    # Convert key from message_tuple to 128-bit binary string
     key = bin(mt.key)[2:].zfill(128)
+
+    # Initialize AES cipher with key and message
     cipher = AES(key, mt.message)
     
     print("Key:", key)
     print(f"Message: {mt.message}\n")
 
-    encrypted_message = cipher.encrypt(cipher.key, cipher.message)
+    # Print the original message in table format
     print("Original Message:")
     message_block = cipher._message_table(cipher.message)
     for row in message_block:
         print(row)  
     print()
+
+    # Encrypt the message 
+    encrypted_message = cipher.encrypt(cipher.key, cipher.message)
+    # Print the encrypted message in table format
     print("Encrypted Message Table:")
     for row in encrypted_message:
         print(row)  
     print()
         
+    # Convert the encrypted message from binary to hexadecimal string format
     encrypted_message = cipher.encrypt(cipher.key, cipher.message)
     
     ciphertext = ""
@@ -273,15 +321,6 @@ def main():
             byte_hex = hex(byte_int)[2:].zfill(2)
             ciphertext += byte_hex
     print("Ciphertext:", ciphertext)
-        
-            
-    
-    # cipher_table = cipher.message_table(cipher.message)
-    # print("Cipher Table:")
-    # print(cipher_table)
-    # shifted_table = cipher.shift_rows(cipher_table)
-    # print("Shifted Table:")
-    # print(shifted_table)
     
 if __name__ == "__main__":
     main()
